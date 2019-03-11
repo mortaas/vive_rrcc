4aaa0f97875522468a459cff37d1982b7f2dc1dc
Unknown <mortaas@stud.ntnu.no>
2019-03-11 19:14:37 +0100

    Initial implementation of cuboid sampling with VIVE Controller + visualization

--- vive_planning_scene/src/vive_scene_node.cpp
+++ vive_planning_scene/src/vive_scene_node.cpp
@@ -1,3 +1,5 @@
+#include <signal.h>
+
 // ROS
 #include <ros/ros.h>
 // ROS msgs
@@ -21,6 +23,15 @@
 // SDFormat
 // #include <sdf/sdf.hh>
 
+
+// Handle signal [ctrl + c]
+bool sigint_flag = true;
+
+void IntHandler(int signal) {
+    sigint_flag = false;
+}
+
+
 class SceneNode {
     ros::NodeHandle nh_;
     ros::Rate loop_rate_;
@@ -39,7 +50,7 @@ class SceneNode {
     // moveit_msgs::ApplyPlanningScene planning_scene_srv_;
     // moveit_msgs::PlanningScene planning_scene_;
 
-    rviz_visual_tools::RvizVisualToolsPtr visual_tools_;
+    rviz_visual_tools::RvizVisualToolsPtr rviz_tools_;
 
     // SDFormat
     // sdf::SDFPtr sdf_;
@@ -49,14 +60,14 @@ class SceneNode {
     tf2_ros::TransformListener *tf_listener_;
 
     tf2::Transform tf_tracker_;
-    std::string controller_frame, tracker_frame;
+    std::string controller_frame, world_frame;
 
     // Eigen
-    Eigen::Vector3d point1, point2, point3;
-    Eigen::Vector3d x_basis_, y_basis_, z_basis_;
-    Eigen::Affine3d eigen_pose_, eigen_eurobox_center_;
+    Eigen::Vector3d points_[8], basis_[4];
+    Eigen::Affine3d eigen_pose_, eigen_controller_offset_; //, eigen_eurobox_center_;
     
     int state;
+    double height;
 
     public:
         SceneNode(int frequency);
@@ -76,9 +87,9 @@ void SceneNode::DevicesCb(const vive_bridge::TrackedDevicesStamped& msg_) {
         if (msg_.device_classes[i] == msg_.CONTROLLER) {
             controller_frame = msg_.device_frames[i];
         }
-        if (msg_.device_classes[i] == msg_.TRACKER) {
-            tracker_frame = msg_.device_frames[i];
-        }
+        // if (msg_.device_classes[i] == msg_.TRACKER) {
+        //     tracker_frame = msg_.device_frames[i];
+        // }
     }
 }
 
@@ -92,18 +103,24 @@ SceneNode::SceneNode(int frequency)
     // planning_scene_client_ = nh_.serviceClient<moveit_msgs::ApplyPlanningScene>("apply_planning_scene");
     // planning_scene_client_.waitForExistence();
 
-    visual_tools_.reset(new rviz_visual_tools::RvizVisualTools("world", "/rviz_visual_markers") );
+    rviz_tools_.reset(new rviz_visual_tools::RvizVisualTools("world", "/rviz_visual_markers") );
 
     // SDFormat
     // sdf_root_ = sdf_->Root();
 
-    // Eigen
-    point1.setZero();
-    point2.setZero();
-    point3.setZero();
+    double angle_offset = -std::atan((0.027553 - 0.00985)/(0.025907 + 0.002273) );
 
-    eigen_eurobox_center_.matrix().setIdentity();
-    eigen_eurobox_center_.matrix().block<3, 1>(0, 3) = Eigen::Vector3d(-0.189224, -0.139224, -0.074999);
+    eigen_controller_offset_.rotate(Eigen::Quaterniond(0., 0., 1., 0.) *
+                                    Eigen::Quaterniond(std::sin(angle_offset/2), 0., 0., std::cos(angle_offset/2) ) );
+    eigen_controller_offset_.translate(Eigen::Vector3d(0., -0.036594, 0.00955) );
+
+    // Eigen
+    // point1.setZero();
+    // point2.setZero();
+    // point3.setZero();
+
+    // eigen_eurobox_center_.matrix().setIdentity();
+    // eigen_eurobox_center_.matrix().block<3, 1>(0, 3) = Eigen::Vector3d(-0.189224, -0.139224, -0.074999);
 
     state = 0;
 }
@@ -119,48 +136,58 @@ void SceneNode::JoyCb(const sensor_msgs::Joy& msg_) {
         switch (state) {
             case 0:
                 ROS_INFO("Point 1!");
-                ROS_INFO_STREAM(point1.matrix() );
+                ROS_INFO_STREAM(points_[0].matrix() );
                 state = 1;
                 break;
             case 1:
                 ROS_INFO("Point 2!");
-                ROS_INFO_STREAM(point2.matrix() );
+                ROS_INFO_STREAM(points_[1].matrix() );
                 state = 2;
+
+                basis_[0] = points_[1] - points_[0];
                 break;
             case 2:
                 ROS_INFO("Point 3!");
-                ROS_INFO_STREAM(point3.matrix() );
+                ROS_INFO_STREAM(points_[2].matrix() );
                 state = 3;
                 break;
             case 3:
-                ROS_INFO("Publish eurobox!");
-
-                // Compute basis vectors from points
-                x_basis_ = (point1 - point2);
-                x_basis_ = x_basis_ / x_basis_.norm();
-                
-                y_basis_ = (point1 - point3);
-                y_basis_ = y_basis_ / y_basis_.norm();
-
-                z_basis_ = x_basis_.cross(y_basis_);
-
-                // Create pose from basis vectors (rotation) and first point (translation)
-                eigen_pose_.matrix().block<3, 1>(0, 0) = x_basis_;
-                eigen_pose_.matrix().block<3, 1>(0, 1) = y_basis_;
-                eigen_pose_.matrix().block<3, 1>(0, 2) = z_basis_;
-                eigen_pose_.matrix().block<3, 1>(0, 3) = point1;
-                ROS_INFO_STREAM(eigen_pose_.matrix() );
-
-                visual_tools_->publishMesh(eigen_pose_ * eigen_eurobox_center_,
-                                           "package://clean_grasps/workcell_support/urdf/eurobox/meshes/eurobox.STL",
-                                           rviz_visual_tools::GREY,
-                                           1.0,
-                                           "Eurobox");
-                visual_tools_->trigger();
-
-                // Reset to first point
+                ROS_INFO("Point 4!");
+                ROS_INFO_STREAM(points_[3].matrix() );
+                state = 4;
+                break;
+            case 4:
+                ROS_INFO("Publish!");
+                state = 5;
+                break;
+            case 5:
                 state = 0;
                 break;
+
+                // // Compute basis vectors from points_
+                // basis_[0] = (points_[0] - points_[1]);
+                // basis_[0] = basis_[0] / basis_[0].norm();
+                
+                // basis_[1] = (points_[0] - points_[2]);
+                // basis_[1] = basis_[1] / basis_[1].norm();
+
+                // basis_[2] = basis_[0].cross(basis_[1]);
+
+                // // Create pose from basis vectors (rotation) and first point (translation)
+                // eigen_pose_.matrix().block<3, 1>(0, 0) = basis_[0];
+                // eigen_pose_.matrix().block<3, 1>(0, 1) = basis_[1];
+                // eigen_pose_.matrix().block<3, 1>(0, 2) = basis_[2];
+                // eigen_pose_.matrix().block<3, 1>(0, 3) = point1;
+                // ROS_INFO_STREAM(eigen_pose_.matrix() );
+
+                // rviz_tools_->publishMesh(eigen_pose_ * eigen_eurobox_center_,
+                //                            "package://clean_grasps/workcell_support/urdf/eurobox/meshes/eurobox.STL",
+                //                            rviz_visual_tools::GREY,
+                //                            1.0,
+                //                            "Eurobox");
+                // rviz_tools_->trigger();
+
+                // Reset to first point
         }
     }
 }
@@ -170,60 +197,111 @@ bool SceneNode::Init() {
      * Check if the necessary transforms are available and initialize the node
      */
 
-    while (controller_frame.empty() && tracker_frame.empty() ) {
-        ROS_INFO("Waiting for controller and tracker...");
+    nh_.param<std::string>("/vive_node/world_frame", world_frame, "root");
+
+    while (controller_frame.empty() ) {
+        ROS_INFO("Waiting for controller...");
         
         ros::spinOnce();
         ros::Duration(5.0).sleep();
     }
-    ROS_INFO_STREAM("Using " + controller_frame + " and " + tracker_frame + " for calibration");
+    ROS_INFO_STREAM("Using " + controller_frame + " for calibration");
 
     joy_sub_ = nh_.subscribe("/vive_node/joy/" + controller_frame, 1, &SceneNode::JoyCb, this);
 
     std::string pError;
-    if (!tf_buffer_.canTransform(tracker_frame,
-                                 "world", ros::Time(0),
+    if (!tf_buffer_.canTransform(controller_frame,
+                                 world_frame, ros::Time(0),
                                  ros::Duration(10.0), &pError) )
     {
-        ROS_ERROR_STREAM("Can't transform from world to " + tracker_frame + ": " + pError);
+        ROS_ERROR_STREAM("Can't transform from " + world_frame + " to " + controller_frame + ": " + pError);
 
         return false;
     }
 
     // planning_scene_.is_diff = true;
 
-    // visual_tools_->setLifetime(0.01);
-    visual_tools_->setBaseFrame("world");
+    rviz_tools_->setLifetime(0.1);
+    rviz_tools_->setBaseFrame(world_frame);
     
     return true;
 }
 
 void SceneNode::Loop() {
-    tf_msg_ = tf_buffer_.lookupTransform("world", tracker_frame, ros::Time(0) );
+    tf_msg_ = tf_buffer_.lookupTransform(world_frame, controller_frame, ros::Time(0) );
 
     switch (state) {
         case 0:
-            point1(0, 0) = tf_msg_.transform.translation.x;
-            point1(1, 0) = tf_msg_.transform.translation.y;
-            point1(2, 0) = tf_msg_.transform.translation.z;
+            points_[0](0, 0) = tf_msg_.transform.translation.x;
+            points_[0](1, 0) = tf_msg_.transform.translation.y;
+            points_[0](2, 0) = tf_msg_.transform.translation.z;
+
+            rviz_tools_->resetMarkerCounts();
+            rviz_tools_->publishSphere(points_[0], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->trigger();
             break;
         case 1:
-            point2(0, 0) = tf_msg_.transform.translation.x;
-            point2(1, 0) = tf_msg_.transform.translation.y;
-            point2(2, 0) = tf_msg_.transform.translation.z;
+            points_[1](0, 0) = tf_msg_.transform.translation.x;
+            points_[1](1, 0) = tf_msg_.transform.translation.y;
+            points_[1](2, 0) = tf_msg_.transform.translation.z;
+
+            rviz_tools_->resetMarkerCounts();
+            rviz_tools_->publishSphere(points_[0], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[1], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishLine(points_[0], points_[1], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->trigger();
             break;
         case 2:
-            point3(0, 0) = tf_msg_.transform.translation.x;
-            point3(1, 0) = tf_msg_.transform.translation.y;
-            point3(2, 0) = tf_msg_.transform.translation.z;
+            points_[2](0, 0) = tf_msg_.transform.translation.x;
+            points_[2](1, 0) = tf_msg_.transform.translation.y;
+            points_[2](2, 0) = tf_msg_.transform.translation.z;
+
+            basis_[1] = points_[2] - points_[0];
+            basis_[2] = basis_[0].cross(basis_[1]);
+
+            eigen_pose_.matrix().block<3, 1>(0, 0) = basis_[0] / basis_[0].norm();
+            eigen_pose_.matrix().block<3, 1>(0, 1) = basis_[1] / basis_[1].norm();
+            eigen_pose_.matrix().block<3, 1>(0, 2) = basis_[2] / basis_[2].norm();
+            eigen_pose_.matrix().block<3, 1>(0, 3) = points_[0] + 0.5*(basis_[0] + basis_[1]);
+
+            rviz_tools_->resetMarkerCounts();
+            rviz_tools_->publishSphere(points_[0], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[1], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[2], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishCuboid(eigen_pose_, basis_[0].norm(), basis_[1].norm(), 0.01);
+            rviz_tools_->trigger();
+        case 3:
+            points_[3](0, 0) = tf_msg_.transform.translation.x;
+            points_[3](1, 0) = tf_msg_.transform.translation.y;
+            points_[3](2, 0) = tf_msg_.transform.translation.z;
+
+            height = (points_[3] - points_[0]).dot(basis_[2] / basis_[2].norm() );
+            eigen_pose_.matrix().block<3, 1>(0, 3) = points_[0] + 0.5*(basis_[0] + basis_[1] + basis_[2] / basis_[2].norm() * height);
+
+            rviz_tools_->resetMarkerCounts();
+            rviz_tools_->publishSphere(points_[0], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[1], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[2], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[3], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishCuboid(eigen_pose_, basis_[0].norm(), basis_[1].norm(), std::abs(height) );
+            rviz_tools_->trigger();
+            break;
+        case 4:
+            rviz_tools_->resetMarkerCounts();
+            rviz_tools_->publishSphere(points_[0], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[1], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[2], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishSphere(points_[3], rviz_visual_tools::BLUE, rviz_visual_tools::MEDIUM);
+            rviz_tools_->publishCuboid(eigen_pose_, basis_[0].norm(), basis_[1].norm(), std::abs(height) );
+            rviz_tools_->trigger();
             break;
     }
     
-    // Realtime visualisation of box based on two points
+    // Realtime visualisation of box based on two points_
     // if (state > 0) {
-    //     visual_tools_->resetMarkerCounts();
-    //     visual_tools_->publishCuboid(point1, point2);
-    //     visual_tools_->trigger();
+    //     rviz_tools_->resetMarkerCounts();
+    //     rviz_tools_->publishCuboid(point1, point2);
+    //     rviz_tools_->trigger();
     // }
 
     ros::spinOnce();
@@ -235,13 +313,16 @@ void SceneNode::Shutdown() {
 int main(int argc, char** argv) {
     ros::init(argc, argv, "vive_scene_node");
 
+    // Handle signal [ctrl + c]
+    signal(SIGINT, IntHandler);
+
     SceneNode node_(60);
 
     if (!node_.Init() ) {
         return 0;
     }
 
-    while (ros::ok() ) {
+    while (ros::ok() && sigint_flag) {
         node_.Loop();
     }
 
